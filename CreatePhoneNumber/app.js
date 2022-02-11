7405f5b4d0a02bc42798c7395fa96783dcf23c6f
shanthikaruna <shanthi.karuna01@gmail.com>
2022-02-10 16:41:19 -0800

    CreatePhoneNumbers

---
+++ CreatePhoneNumber/app.js
@@ -0,0 +1,23 @@
+/*codewars kata -Create Phone Number*/
+function createPhoneNumber(arr){
+    //var str = arr.join('')
+
+    //using slice and join method
+
+    return `(${arr.slice(0,3).join('')}) ${arr.slice(3,6).join('')} -${arr.slice(6).join('')}`
+
+    //using join and then substring
+    /*return `(${str.substring(0,3)}) ${str.substring(3,6)} -${str.substring(6)}`*/
+
+    /*using replace method*/
+    /*var mask = 'xxx xxx -xxxx'
+
+    for(var i=0; i<arr.length; i++) {
+      mask= mask.replace('x',arr[i])
+    }
+    return mask*/
+
+}
+
+
+ console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
